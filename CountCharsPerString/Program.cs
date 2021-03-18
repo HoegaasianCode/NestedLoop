3d273aa6e062290b27e3e12e88f08ec56d879751
JoakimHoegaas <62018628+JoakimHoegaas@users.noreply.github.com>
2021-03-18 13:38:09 +0100

    Initial commit

---
+++ CountCharsPerString/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountCharsPerString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = { "one", "two", "three" };
+            List<int> intList = new();
+            int i = 0;
+            foreach (var word in words)
+            {
+                foreach (var character in word)
+                {
+                    i++;
+                }
+                intList.Add(i);
+                i = 0;
+            }
+            Print(intList);
+        }
+
+        private static void Print(List<int> intList)
+        {
+            foreach (var stringCount in intList)
+            {
+                Console.WriteLine(stringCount);
+            }
+        }
+    }
+}
